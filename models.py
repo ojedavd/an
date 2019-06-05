651294c7f7c45bd97bfa048c269ff6328e3c9589
Victor Ojeda <ojedavd@gmail.com>
2019-06-05 14:22:28 -0300

    Clase TA y Menu

--- models.py
+++ models.py
@@ -2,7 +2,9 @@
 
 from openerp import models, fields, api
 
-# class an(models.Model):
-#     _name = 'an.an'
+class Ta(models.Model):
 
-#     name = fields.Char()
+    _name = 'an.ta'  #  Model odoo name
+
+    cve = fields.Boolean(string='Circuito de Verificación de Equipos')
+    cve_operario = fields.Many2one('res.partner', string="Operario")
